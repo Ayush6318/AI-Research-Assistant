b13430c3d1be1fa96a043af5439fc3cb2c4e624c
Ayush kumar <aaushk2005@gmail.com>
2026-06-13 13:47:38 +0530

    rag_answer.py chnages

--- rag/rag_answer.py
+++ rag/rag_answer.py
@@ -1,24 +1,25 @@
 from rag.vector_store import load_vector_store
 from rag.retriever import get_retriever
 
-
 def retrieve_context(question):
 
     vector_store = load_vector_store()
 
-    retriever = get_retriever(
-        vector_store
-    )
+    retriever = get_retriever(vector_store)
 
-    docs = retriever.invoke(
-        question
-    )
+    docs = retriever.invoke(question)
 
-    context = "\n\n".join(
-        [
-            doc.page_content
-            for doc in docs
-        ]
-    )
+    context = "\n\n".join([doc.page_content for doc in docs])
 
     return context
+
+def retrieve_context_with_score(question, k=3):
+
+    vector_store = load_vector_store()
+
+    results = vector_store.similarity_search_with_score(
+        question,
+        k=k
+    )
+
+    return results
